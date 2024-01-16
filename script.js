1bb3f4fb62c1aa1efa8718cb6d5ac6cfa72544d5
sally-1994 <150714749+sally-1994@users.noreply.github.com>
2024-01-16 00:37:34 +0000

    add js ls01

---
+++ script.js
@@ -0,0 +1,31 @@
+let usernumber = 100;
+if(usernumber < 50) {
+    console.log("naklebia 50-ze");
+}
+else if (usernumber > 20){
+    console.log( "metia 20-ze");
+}
+ else{
+console.log(error);
+}
+
+let username = "Mariam";
+if(username = "Mariam"){
+    console.log(true);
+}else if(username = "salome"){
+console.log(false);}
+else{
+    console.log(error)
+}
+
+switch (username){
+    case "mariam":
+        console.log(true);
+        break;
+        case "salome":
+            console.log(false);
+            break;
+            default:
+                console.log(false)
+}
+
